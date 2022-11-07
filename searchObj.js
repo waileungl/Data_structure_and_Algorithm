9fd2385d44996c5fcf91ed655056b76a22c3a325
William Liu <110699040+waileungl@users.noreply.github.com>
2022-11-07 10:12:56 -0800

    11/7

---
+++ searchObj.js
@@ -0,0 +1,26 @@
+function findObjectsFilter(search, items) {
+    const searchKeys = Object.keys(search);
+
+    return items.filter(item => {
+        for (const key of searchKeys) {
+            if (item[key] !== search[key]) {
+                return false;
+            }
+        }
+        return true;
+    })
+}
+
+const items = [
+    { firstName: "Bob", lastName: "Bobbert", age: 31 },
+    { firstName: "Bob", lastName: "Roberts", age: 20 },
+    { firstName: "Bob", lastName: "Bill", age: 27 },
+    { firstName: "Bob", lastName: "Smith", age: 31 }
+]
+
+const SearchFor = {
+    firstName: "Bob",
+    age: 31
+}
+
+console.log(findObjectsFilter(SearchFor, items))
