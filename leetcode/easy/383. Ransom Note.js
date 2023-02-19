f9321a0c5ffb6fd904866fadcaf51f293e468fef
William Liu <110699040+waileungl@users.noreply.github.com>
2023-02-18 22:07:27 -0800

    new

--- leetcode/easy/383. Ransom Note.js
+++ leetcode/easy/383. Ransom Note.js
@@ -15,7 +15,6 @@
 // Input: ransomNote = "aa", magazine = "aab"
 // Output: true
 
-////
 // Constraints:
 // 1 <= ransomNote.length, magazine.length <= 105
 // ransomNote and magazine consist of lowercase English letters.
