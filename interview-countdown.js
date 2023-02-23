caa68e26d9fd1e16688d60d8d856fc113e991f4e
William Liu <110699040+waileungl@users.noreply.github.com>
2023-02-23 03:38:59 -0800

    new

---
+++ interview-countdown.js
@@ -0,0 +1,23 @@
+function CountDownButton() {
+    const [count, setCount] = useState(10);
+    const [isPaused, setIsPaused] = useState(false);
+
+    useEffect(() => {
+        let intervalId;
+        if (!isPaused && count > 0) {
+            intervalId = setInterval(() => {
+                setCount(count => count - 1), 1000
+            });
+        }
+        return () => clearInterval(intervalId); //closure 
+    }, [isPaused]);
+
+
+    return (
+        <button onClick={() => {
+            setIsPaused(prev => !prev);
+        }}>
+            {isPaused ? 'Resume' : 'Pause'} Count Down: {count}
+        </button>
+    );
+}
