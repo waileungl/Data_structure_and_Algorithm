4ad1e51212da54e37de26e668890b68c5298dd97
William Liu <110699040+waileungl@users.noreply.github.com>
2023-03-29 19:41:39 -0700

    new

--- interview/Intuit.js
+++ interview/Intuit.js
@@ -5,7 +5,7 @@
 sum of all the differences of max and min on each row = 26 */
 
 function arrChecker(...arr) {
-    return arr.reduce((acc, curr) =>  acc + (Math.max(...curr) - Math.min(...curr)), 0)
+    return arr.reduce((acc, curr) => acc + (Math.max(...curr) - Math.min(...curr)), 0)
 }
 
 // console.log(arrChecker([1, 2, 13, 8], [10, 4, 7, 5], [5, 3, 7, 11]))
@@ -17,27 +17,29 @@ function arrChecker(...arr) {
 {5,3,6,11} 6%3 = 0, 6/3 = 2
 sum of all the result 6 + 2 + 2 = 10 */
 
-function arrChecker2(...arr){
+function arrChecker2(...arr) {
     let sum = 0;
     arr.forEach(item => {
-        let map ={}
-        for(let i in item){
-            if(getFactors(item[i]).includes()){
-
-            }
+        for (let i in item) {
+            let factors = getFactors(item[i])
+            factors.forEach(factor => {
+                if (item.includes(factor)) {
+                    sum += (item[i] / factor)
+                }
+            })
         }
     })
     return sum;
 }
 
-function getFactors(num){
+function getFactors(num) {
     let arr = new Array();
-    for(let i = 1; i < num; i++){
-        if(num%i ===0){
+    for (let i = 1; i < num; i++) {
+        if (num % i === 0) {
             arr.push(i)
         }
     }
     return arr
 }
 
-console.log(arrChecker2([11,2,12,9],[10,4,7,5],[5,3,6,11]))
+console.log(arrChecker2([11, 2, 12, 9], [10, 4, 7, 5], [5, 3, 6, 11]))
