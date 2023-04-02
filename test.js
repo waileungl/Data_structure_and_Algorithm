ce5e3146066fe5c49f077f425b33f24ee5319283
William Liu <110699040+waileungl@users.noreply.github.com>
2023-04-01 23:55:51 -0700

    new

--- test.js
+++ test.js
@@ -1,16 +1,20 @@
-function sum(num1) {
-    function sum1(num2, currSum = res) {
-        return sum(num1 + num2)
+var countAndSay = function (n) {
+    let base = "1"
+    while (n > 1) {
+        let say = ""
+        let count = 1
+        for (let i =0; i < base.length; i++) {
+            if (base[i] == base[i + 1]) {
+                count++
+            } else {
+                say += (count + base[i])
+                count = 1
+            }
+        }
+        base = say
+        n--;
     }
+    return base
+};
 
-    sum1.toString = () => num1
-    return res
-}
-
-
-// const sum1 = sum(1)
-// console.log(sum1(2)) // true == 3
-// console.log(sum1(3)) // true == 4
-console.log(sum(1)(2)(3)) // true == 6
-// console.log(sum(5)(-1)(2))  // true == 6)
-
+console.log(countAndSay(4))
