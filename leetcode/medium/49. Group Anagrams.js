b11fffd96c4b49224b3974684822acb16af3ef19
William Liu <110699040+waileungl@users.noreply.github.com>
2023-04-06 01:38:17 -0700

    new

---
+++ leetcode/medium/49. Group Anagrams.js
@@ -0,0 +1,46 @@
+var groupAnagrams = function (strs) {
+    if (strs.length === 1) return [strs]
+    let map = {};
+    for (let i in strs) {
+        let containAna = "check";
+
+        Object.keys(map).forEach(item => {
+            if (isAnagram(strs[i], item)) {
+                containAna = item
+            }
+        })
+        if (containAna !== "check") {
+            map[containAna].push(strs[i])
+        } else {
+            map[strs[i]] = [strs[i]]
+        }
+    }
+    return Object.values(map)
+};
+
+const isAnagram = (s, t) => {
+    if (s.length !== t.length) return false
+    if(s.length === 0) return true
+
+    let map = {}
+
+
+    for (let i of s) {
+        if (map[i] !== 0) {
+            map[i]++
+        } else {
+            map[i] = 1
+        }
+    }
+
+    console.log(map)
+    for (let j of t) {
+        if (!map[j]) {
+            return false
+        } else {
+            map[j]--
+        }
+    }
+    return Math.max(...Object.values(map)) === 0
+}
+console.log(groupAnagrams(["", ""]))
--- test.js
+++ test.js
@@ -1,18 +1,1 @@
-var coinChange = function (coins, amount) {
-    if(amount === 0) return 0
-    if (coins.length === 1) {
-        if (amount % coins[0] === 0) {
-            return amount/coins[0]
-        }  else {
-            return -1
-        }
-    }
-
-    let max = Math.max(...coins)
-    if (coins.includes(amount % max)) {
-        return Math.floor(amount / max) + 1
-    }else{
-        coins.pop()
-        coinChange(coins, amount)
-    }
-};
+console.log(`${(item => [...item, 9])([7, 8])}heyyy`)
