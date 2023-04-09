5c14334c0593ac474f2b0919dcecd6b815338e73
William Liu <110699040+waileungl@users.noreply.github.com>
2023-04-08 21:51:38 -0700

    Sup

--- test.js
+++ test.js
@@ -1,3 +1,26 @@
-let t = "ABC"
+var search = function(nums, target) {
+    if(nums[0] === target) return 0
+    let l = 0
+    let r = nums.length - 1
+    while(l < r){
+        let mid = Math.floor((l + r)/2)
+        if(nums[mid] === target){
+            return mid
+        }
 
-console.log("BAN".includes(...t.split("")))
+        if(nums[mid] > nums[l]){
+            if(target < nums[mid] && target > nums[l]){
+                r = mid - 1
+            }else{
+                l = mid + 1
+            }
+        } else{
+            if(target > nums[mid] && target < nums[r]){
+                l = mid - 1
+            }else{
+                r = mid + 1
+            }
+        }
+    }
+    return -1
+};
