0c31eb346cab103911bc917e754af65e4c8fb5b1
William Liu <110699040+waileungl@users.noreply.github.com>
2023-04-10 11:31:12 -0700

    hi

--- test.js
+++ test.js
@@ -1,26 +1,4 @@
-var search = function(nums, target) {
-    if(nums[0] === target) return 0
-    let l = 0
-    let r = nums.length - 1
-    while(l < r){
-        let mid = Math.floor((l + r)/2)
-        if(nums[mid] === target){
-            return mid
-        }
+const name = 'Lydia Hallie';
 
-        if(nums[mid] > nums[l]){
-            if(target < nums[mid] && target > nums[l]){
-                r = mid - 1
-            }else{
-                l = mid + 1
-            }
-        } else{
-            if(target > nums[mid] && target < nums[r]){
-                l = mid - 1
-            }else{
-                r = mid + 1
-            }
-        }
-    }
-    return -1
-};
+console.log(!typeof name === 'object');
+console.log(!typeof name === 'string');
