571e87d661c77beca2a327039a574dffc8c82330
William Liu <110699040+waileungl@users.noreply.github.com>
2023-04-11 17:23:19 -0700

    new

--- test.js
+++ test.js
@@ -1,4 +1,15 @@
-const name = 'Lydia Hallie';
+var findDuplicate = function(nums) {
+    let map = new Object()
+    for(const i of nums){
+        map[i]? map[i]++ : map[i] = 1
+    }
+    
+    for(const[key, values] of Object.entries(map)){
+        console.log(key, values)
+        if(values > 1) return key
+    }
 
-console.log(!typeof name === 'object');
-console.log(!typeof name === 'string');
+};
+
+
+console.log(findDuplicate([3,1,3,4,2]))
