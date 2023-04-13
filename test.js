14fee6acd32c61924eea88092e02a059f6c3b5ec
William Liu <110699040+waileungl@users.noreply.github.com>
2023-04-12 23:51:38 -0700

    new

--- test.js
+++ test.js
@@ -1,15 +1,4 @@
-var findDuplicate = function(nums) {
-    let map = new Object()
-    for(const i of nums){
-        map[i]? map[i]++ : map[i] = 1
-    }
-    
-    for(const[key, values] of Object.entries(map)){
-        console.log(key, values)
-        if(values > 1) return key
-    }
-
-};
+const kai = "123"
 
 
-console.log(findDuplicate([3,1,3,4,2]))
+console.log(parseInt(kai) + 123)
