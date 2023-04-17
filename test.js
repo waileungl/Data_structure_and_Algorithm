9b4b363bca2024f4e1b18172102080d741ec0470
William Liu <110699040+waileungl@users.noreply.github.com>
2023-04-17 16:17:09 -0700

    new

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-const kai = "123"
-//
+const socks = [1, 2, 2, 1, 1, 3, 5, 1, 4, 4]
 
-console.log(parseInt(kai) + 123)
+
+const sockPair = socks => {
+    let map = new Object()
+    for(const sock of socks){
+        map[sock]? map[sock] ++ : map[sock] = 1
+    }
+    return Object.entries(map).filter((item)=> item[1]%2 === 0).reduce((acc, curr) => (acc + curr[1]), 0)/2
+}
+
+console.log(sockPair(socks)) 
